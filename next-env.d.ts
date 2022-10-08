a3abe63238313508e6348cef8fc56cda8d0fad2d
yuweichen1008 <yuweiichen@gmail.com>
2022-10-08 08:30:21 +0800

    update package version

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "cloudinary-react";
-
-type YOLO = any;
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
